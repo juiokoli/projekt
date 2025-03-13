9ab3a12d66aa0b052fb9cac727e3e7d6806bac1a
juiokoli <dominikstanek06@gmail.com>
2025-03-13 08:46:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,15 +13,26 @@ const buybtn = document.querySelectorAll(".buybtn");
 const upgradeCount = document.querySelectorAll("#upgradeCount");
 const upgradeCountArr = [0,0,0,0,0];
 const upgradeTab = document.getElementById("upgradeTab");
+const levelP = document.getElementById("lvl");
+const expP = document.getElementById("exp");
+const creditsNav = document.getElementById("credits");
+const creditsDiv = document.getElementById("creditsDiv");
+const closeCredBtn = document.getElementById("closeBtn");
+const comboGif = document.getElementById("comboGif");
 let showHide = 0;
 settingsTab.id = "settingsTab";
 let clickVal = 1;
 let secVal = 0;
 let points = 0;
-letwhichBtn = 0;
+let upgradePoints = 0;
+let currXp = 0;
+let currLvl = 0;
+let xpToLvl = 100;
 let comboTimeout;
 let clickCounter = 0;
 const price = ["price1","price2","price3","price4","price5"]
+expP.textContent = `Exp: ${currXp}/${xpToLvl}`
+levelP.textContent = `Level: ${currLvl}`
 let upgradeArr = [
     {clickVal:1,secVal:0,price:10},
     {clickVal:0,secVal:5,price:250},
@@ -29,11 +40,20 @@ let upgradeArr = [
     {clickVal:100,secVal:0,price:150000},
     {clickVal:0,secVal:100,price:200000}
 ];
+const level = () => {
+    if (currXp >= xpToLvl) {
+        currLvl++
+        upgradePoints += 1
+        currXp = 0
+        xpToLvl *= 2.5
+    }
+    levelP.textContent = `Level: ${currLvl}`
+}
 const showUpgrades = () => {
    if (showHide === 0) {
     for (let i = 0; i<5;i++) {
         price[i] = document.createElement("p")
-        price[i].textContent = upgradeArr[i].price
+        price[i].textContent = upgradeArr[i].price.toString().length > 10 ? upgradeArr[i].price.toExponential(2) : upgradeArr[i].price ;
         price[i].classList.add("price")
         upgrade[i].appendChild(price[i])
         upgradeTab.style.display = "flex";
@@ -72,34 +92,39 @@ const upgradeChoice = upgradeArr[index];
     clickVal += upgradeChoice.clickVal
     secVal += upgradeChoice.secVal
     points -= Math.ceil(upgradeChoice.price)
-    pointsP.textContent = `Points: ${points}`
+    pointsP.textContent = `Points: ${points.toString().length > 10? points.toExponential(2) : points}`  
     upgradeArr[index].price *= 1.5
-    price[index].textContent = Math.ceil(upgradeArr[index].price)
+    price[index].textContent = Math.ceil(upgradeArr[index].price.toString().length>10 ? upgradeArr[index].price.toExponential(2) : upgradeArr[index].price)
     upgradeCountArr[index]++
     upgradeCount[index].textContent = `Count: ${Number(upgradeCountArr[index])}`
 }
 const combo = () => {
     click.classList.add("combo")
+    comboGif.style.display = "block"
     if (comboTimeout) {
         clearTimeout(comboTimeout)
     }
     comboTimeout = setTimeout(() => {  
         click.classList.remove("combo")
+        comboGif.style.display = "none"
         clickCounter = 0
     },1000)
 }
 showUpgradesBtn.addEventListener("click",showUpgrades)
 const clickFunction = () => {
+    currXp += 1;
+    expP.textContent = `Exp: ${currXp}/${xpToLvl}`
+    level()
     clickCounter++
     if (clickCounter >= 20) {
     combo()
     }
     if (clickCounter >= 20) {
         points += clickVal*2;
-        pointsP.textContent = `Points: ${points}`
+        pointsP.textContent = `Points: ${points.toString().length > 10? points.toExponential(2) : points}`
     } else {
     points += clickVal
-    pointsP.textContent = `Points: ${points}`
+    pointsP.textContent = `Points: ${points.toString().length > 10? points.toExponential(2) : points}`
     }
 };
 click.addEventListener("click",clickFunction);
@@ -127,6 +152,12 @@ settingsNav.addEventListener("click",settings)
 infoNav.addEventListener("click",info)
 const secFunction = () => {
     points += secVal;
-    pointsP.textContent = `Points: ${points}`
+    pointsP.textContent = `Points: ${points.toString().length > 10? points.toExponential(2) : points}`
 };
 setInterval(secFunction,1000)
+creditsNav.addEventListener("click",()=>{
+    creditsDiv.style.display = "flex"
+})
+closeCredBtn.addEventListener("click",()=>{
+    creditsDiv.style.display = "none"
+})
